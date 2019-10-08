6b2a51db5de5298cc60fce754659164b05658490
Richard Patel <me@terorie.dev>
2019-10-08 05:04:05 +0200

    Fix godoc

--- column.go
+++ column.go
@@ -3,7 +3,7 @@ package csv
 // A Column of a CSV row.
 type Column struct {
 	// When used with a Reader, signals whether the field was quoted.
-	// When used with a Writer,
+	// When sent as true to a Writer, forces quotation.
 	Quoted bool
 	// Value of the column
 	Value string
